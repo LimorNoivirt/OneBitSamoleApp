b5d62f41fd96d2009289548e82d1582748a1532b
Aaron Urkin <aaron.urkin@1beat.com>
2022-02-24 16:31:47 +0200

    add session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+     flash.now[:warning] = 'Invalid email/password'
+     render 'new'
+    end
+    
+  end
+
+  def destroy
+  end
+
+
 end
