19d8f3072ec3cf71b5c08df5feb00b7137b40747
Aaron Urkin <aaron.urkin@1beat.com>
2022-02-24 18:24:11 +0200

    save user info in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,13 @@ class SessionsController < ApplicationController
     
   end
 
+  def current_user
+    if session[:user_id]
+      @current_user = @current_user || User.find_by(id: session[:user_id])
+    end
+  end
+
+
   def destroy
   end
 
