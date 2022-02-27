01f49a3cc75c57902a88e7bc514281cafc94e672
Aaron Urkin <aaron.urkin@1beat.com>
2022-02-27 13:40:06 +0200

    add tests to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,24 @@
 class SessionsController < ApplicationController
+
+
+
   def new
   end
 
   def create
-    user = User.find(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-     flash.now[:warning] = 'Invalid email/password'
-     render 'new'
-    end
-    
+    user = User.find(email: params[:session][:email].downcase)
+     if user && user.authenticate(params[:session][:password])
+       log_in user
+       redirect_to user
+     else
+      flash.now[:warning] = 'Invalid email/password'
+      render 'new'
+     end
   end
 
   def current_user
     if session[:user_id]
-      @current_user = @current_user || User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
