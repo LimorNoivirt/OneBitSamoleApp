a22d1e61467bf2689233ddec5404b5d606a3ff71
Aaron Urkin <aaron.urkin@1beat.com>
2022-03-03 12:43:08 +0200

    reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   
   root 'static_pages#home'
   get 'sessions/new'
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
